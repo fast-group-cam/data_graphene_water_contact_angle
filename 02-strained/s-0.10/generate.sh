0c018e687a4f2d2f8b6d9c79b2df2da4a0807b83
dwl38 <dwl38@cam.ac.uk>
2026-02-04 13:11:45 +0000

    Transferred contents of 02-strained from working directory to repository

---
+++ 02-strained/s-0.10/generate.sh
@@ -0,0 +1,6 @@
+source ../../00-common/software/venv/bin/activate
+python ../../00-common/software/scripts/stretch-graphene.py \
+    ../../01-unstrained/mace/fixed-1000-molecules/equi_final.lammps-data \
+    -s="-0.001" \
+    -o start.lammps-data
+deactivate
